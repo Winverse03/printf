1d8fc7439a31daa49cb090a313fb311bc01fd469
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-20 03:30:40 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,5 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+/**
+ * struct printstruct - structure containing
+ * @c: the location and method to translate data to characters.
+ * @n: print function for specific type.
+ * Return: int
+ */
+
+typedef struct printstruct
+{
+	char *c;
+	int (*n)(char *format, va_list);
+} structype;
 
 int _printf(const char *format, ...);
+
 #endif
