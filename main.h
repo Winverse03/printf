949f6f62c824509bc4bf30ab595eb846c73617fe
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-22 16:49:59 +0100

    main header

--- main.h
+++ main.h
@@ -1,22 +1,30 @@
-#ifndef _MAIN_H_
-#define _MAIN_H_
-#include <stdarg.h>
-#include <stddef.h>
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <limits.h>
+#include <string.h>
 
 /**
- * struct printstruct - structure containing
- * @c: the location and method to translate data to characters.
- * @n: print function for specific type.
- * Return: int
+ * struct func_type - type structure
+ * @t: pointer to the argument
+ * @f: pointer-function associated with the argumen
  */
-
-typedef struct printstruct
+typedef struct func_type
 {
-	char *c;
-	int (*n)(char *format, va_list);
-} structype;
+	char *t;
+	int (*f)(va_list);
+} func_t;
 
+int (*get_func(const char *format))(va_list);
+int _putchar(char c);
+int _printf(const char *format, ...);
+int print_str(va_list args);
+int print_char(va_list args);
+int print_pct(va_list args);
+int print_dec(va_list args);
 int _printf(const char *format, ...);
 int _putchar(char c);
 
