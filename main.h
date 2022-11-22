ac81bd2e9e31dda163006dfa21a2da7b6d166213
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-22 14:19:40 +0100

    Update main.h

--- main.h
+++ main.h
@@ -1,36 +1,23 @@
 #ifndef _MAIN_H_
 #define _MAIN_H_
-
 #include <stdarg.h>
 #include <stddef.h>
 #include <stdlib.h>
 
 /**
- * struct structprint - structure containing
- * @q: the location and method to translate data to characters.
- * @u: print function for specific type.
+ * struct printstruct - structure containing
+ * @c: the location and method to translate data to characters.
+ * @n: print function for specific type.
  * Return: int
  */
-typedef struct structprint
+
+typedef struct printstruct
 {
-	char *q;
-	int (*u)(char *format, va_list);
+	char *c;
+	int (*n)(char *format, va_list);
 } structype;
 
-int _putchar(char ch);
-int _puts(char *string);
-int printc(char *format, va_list);
-int printstr(char *format, va_list);
-int (*driver(char *format))(char *format, va_list);
-int _printf(char *format, ...);
-int printint(char *format, va_list pa);
-int integer(int number);
-int contadordigit(int number);
-int _abs(int number);
-int printpercent(char *format, va_list pa);
-int printhex(char *format, va_list);
-int printHEX(char *format, va_list);
-int printocta(char *format, va_list);
-int print_unsign(char *format, va_list);
 int _printf(const char *format, ...);
+int _putchar(char c);
+
 #endif
