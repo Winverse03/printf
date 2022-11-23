6b1540f81d37915b38218e1995d7edb1f321ae78
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 03:52:59 +0100

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,36 +1,40 @@
 #include "main.h"
+#include <stdarg.h>
 #include <stdio.h>
 #include <stddef.h>
+#include <limits.h>
 
 /**
- * _printf - produces output according to a format
- * @format: a character string
- * Return: integer number of output
+ * _printf - prints formatted data to stdout
+ * @format: string that contains the format to print
+ * Return: number of characters written
  */
 
-int _printf(const char *format, ...)
+int _printf(const char * const format, ...)
 {
 	va_list nc_print;
-	int i = 0, n_disp = 0, (*func)(va_list);
+	int i = 0, n_disp = 0;
+	int (*func)(va_list);
 
 	va_start(nc_print, format);
-	for (i = 0; format[i] != '\0'; i++)
+
+	while (format[i] != '\0')
 	{
-		if (format[i] != '%')
-		{
-			_putchar(format[i]);
-			n_disp++;
-		}
-		else
+		if (format[i] == '%')
 		{
 			func = _select_func(format[i + 1]);
 			if (func != NULL)
 			{
-				i++;
-				func(nc_print);
-				n_disp++;
+			n_disp += func(nc_print);
+			i++;
 			}
 		}
+		else
+		{
+			_putchar(format[i]);
+			n_disp++;
+		}
+		i++;
 	}
 	va_end(nc_print);
 	return (n_disp);
