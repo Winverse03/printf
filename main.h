a11ace85ba66ad31d6759324c12b775afa06bd86
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 04:04:34 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,18 +4,18 @@
 
 int _putchar(char c);
 int _printf(const char *format, ...);
-int print_int(va_list nc_print);
-int print_char(va_list nc_print);
-int print_str(va_list nc_print);
-int print_bin(va_list nc_print);
-int print_dec(va_list nc_print);
-int print_pct(va_list nc_print);
+int _print_int(va_list args);
+int _print_char(va_list args);
+int _print_str(va_list args);
+int _print_bin(va_list args);
+int _print_dec(va_list args);
+int _print_perc(va_list args);
 int (*_select_func(const char c))(va_list);
-int print_octal(va_list nc_print);
-int print_unint(va_list nc_print);
-int print_x(va_list nc_print);
-int print_X(va_list nc_print);
-int print_hex(unsigned int n, unsigned int c);
+int _print_octal(va_list args);
+int _print_unint(va_list args);
+int _print_x(va_list args);
+int _print_X(va_list args);
+int _print_hex(unsigned int n, unsigned int c);
 
 
 #endif
