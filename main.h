ef250238313f137889301739f175413f406f2341
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 03:44:11 +0100

    Update main.h

--- main.h
+++ main.h
@@ -4,11 +4,11 @@
 #include<stdarg.h>
 int _printf(const char *format, ...);
 int _putchar(char c);
-int print_char(va_list);
-int print_str(va_list);
-int print_int(va_list);
-int print_pct(va_list);
-int print_dec(va_list);
+int print_char(va_list nc_print);
+int print_str(va_list nc_print);
+int print_int(va_list nc_print);
+int print_pct(va_list nc_print);
+int print_dec(va_list nc_print);
 int(*_select_func(char c))(va_list);
 
 #endif
