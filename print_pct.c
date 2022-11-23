aa19536c5ce189a776761177bcd7a33f88811635
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 02:19:52 +0100

    Create print_pct.c

---
+++ print_pct.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_pct - prints character "%"
+ * @n_print : arguments
+ * Return: !
+ */
+int print_pct(va_list n_print)
+{
+	(void)n_print;
+	return (_putchar('%'));
+}
