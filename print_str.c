1f67a8f9b7fe357fa6ddb9fce01bad39f934133a
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 02:29:39 +0100

    Update print_str.c

--- print_str.c
+++ print_str.c
@@ -2,13 +2,13 @@
 
 /**
  * print_str - prints string
- * @n_print: arguments
+ * @nc_print: arguments
  * Return: The number of printed character
  */
-int print_str(va_list n_print)
+int print_str(va_list nc_print)
 {
 	int n = 0;
-	char *str = va_arg(n_print, char *);
+	char *str = va_arg(nc_print, char *);
 
 	if (str == NULL)
 		str = "(null)";
