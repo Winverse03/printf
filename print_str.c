87207e7714b56e27bbbc2172b7da60e31e6b9117
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 04:10:19 +0100

    Update print_str.c

--- print_str.c
+++ print_str.c
@@ -1,18 +1,25 @@
 #include "main.h"
+#include <stdio.h>
+#include <stddef.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * print_str - prints string
- * @nc_print: arguments
- * Return: The number of printed character
+ * _print_str - is a function used to display a string
+ * @args: receives the list of arguments passed
+ * Return: 1
  */
-int print_str(va_list nc_print)
-{
-	int j;
-	char *s = va_arg(nc_print, char *);
 
-	for (j = 0; s[j] != '\0'; j++)
+int _print_str(va_list args)
+{
+	unsigned int k = 0;
+	char *s = va_arg(args, char *);
+
+	if (s == NULL)
+		s = "(null)";
+	for (k = 0; s[k] != '\0'; k++)
 	{
-		_putchar(s[j]);
+		_putchar(s[k]);
 	}
-	return (0);
+	return (k);
 }
