c82287279631021785517c5c77eaf5715e217a6b
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 03:20:26 +0100

    Update _select_func.c

--- _select_func.c
+++ _select_func.c
@@ -1,37 +1,29 @@
 #include "main.h"
-#include <stdarg.h>
 #include <stddef.h>
-#include <unistd.h>
-#include <stdio.h>
 
 /**
- * _select_func - is a function to select respective functions
- * @c: is the input value that holds the conversion specifier
- * Description: the function loops through using an IF statement
- * Return: a pointer to the matching printing function.
+ * _select_func - select functions as per the condition
+ * @c: character for the selection of a function
+ * Return: NULL
  */
-int (*_select_func(const char c))(va_list)
+
+int (*_select_func(char c))(va_list)
 {
 	if (c == 'c')
+	{
 		return (_print_char);
-	else if (c == 's')
+	}
+	if (c == 's')
+	{
 		return (_print_str);
-	else if (c == '%')
-		return (_print_perc);
-	else if (c == 'i')
+	}
+	if (c == 'd')
+	{
 		return (_print_int);
-	else if (c == 'd')
-		return (_print_dec);
-	else if (c == 'b')
-		return (_print_bin);
-	else if (c == 'o')
-		return (_print_octal);
-	else if (c == 'u')
-		return (_print_unint);
-	else if (c == 'x')
-		return (_print_x);
-	else if (c == 'X')
-		return (_print_X);
-
+	}
+	if (c == 'i')
+	{
+		return (_print_int);
+	}
 	return (NULL);
 }
