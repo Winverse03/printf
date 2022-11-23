2c67d3a495fb25869e5a9ddef05cb952d383d142
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 03:39:59 +0100

    Update _select_func.c

--- _select_func.c
+++ _select_func.c
@@ -11,19 +11,19 @@ int (*_select_func(char c))(va_list)
 {
 	if (c == 'c')
 	{
-		return (_print_char);
+		return (print_char);
 	}
 	if (c == 's')
 	{
-		return (_print_str);
+		return (print_str);
 	}
 	if (c == 'd')
 	{
-		return (_print_int);
+		return (print_int);
 	}
 	if (c == 'i')
 	{
-		return (_print_int);
+		return (print_int);
 	}
 	return (NULL);
 }
