e85d4807f0ec20bf3f9b8f7c7ff939b2cab19774
Winverse03 <113660470+Winverse03@users.noreply.github.com>
2022-11-23 04:09:22 +0100

    Update print_pct.c

--- print_pct.c
+++ print_pct.c
@@ -1,12 +1,17 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+#include <unistd.h>
 
 /**
- * print_pct - prints character "%"
- * @n_print : arguments
- * Return: !
+ * _print_perc - is a function to print character
+ * @args: va_list arguements from _printf
+ * Return: number of character printed
  */
-int print_pct(va_list nc_print)
+
+int _print_perc(va_list args)
 {
-	(void)nc_print;
+
+	(void)args;
 	return (_putchar('%'));
 }
